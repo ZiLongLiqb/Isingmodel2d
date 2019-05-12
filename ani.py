2042f16b24ca9e0ed0bb771e151a548a8dde8d01
zilong <1413135@mail.nankai.edu.cn>
2019-05-12 17:18:41 +0800

    update the method to find Tc

--- ani.py
+++ ani.py
@@ -6,6 +6,7 @@ from ffmpy import FFmpeg
 from PIL import Image
 import math
 from scipy import interpolate
+from scipy.optimize import curve_fit
 
 '''outfile="my.gif";
 fig=plt.figure();
@@ -15,12 +16,11 @@ with open("result_xx.txt","r") as f:
 	a=result.split();
 	a=np.array(a,dtype="int");
 
-for i in range(1000):
+for i in range(150):
 	im=plt.imshow(a[i*3600:(i+1)*3600].reshape((60,60)),cmap="binary",animated=True);
 	ims.append([im]);
 
-anim=animation.ArtistAnimation(fig,ims,interval=20,blit=True,repeat_delay=1000)
-anim.save("movie.gif",writer="pillow");
+anim=animation.ArtistAnimation(fig,ims,interval=50,blit=True,repeat_delay=1000)
 plt.show()'''
 
 #plt.imshow(a[0:3600].reshape((60,60)),cmap="binary");
@@ -62,26 +62,6 @@ for i in range(200):
 	length.append(len(d));
 	b.append(d);
 
-'''for i in range(len(b)):
-	ave=0
-	squ=0
-	for j in range(len(b[i])):
-		ave+=b[i][j];
-		squ+=pow(b[i][j],2)
-	ave=ave/len(b[i]);
-	squ=squ/len(b[i]);
-	m_ave.append(pow(ave,2));
-	m_squ.append(squ);
-result=[]
-T=[]
-for i in range(50):
-	result.append(-(m_ave[i]-m_squ[i])/pow(2.2+0.008*i,2));
-	T.append(2.2+0.008*i)
-
-plt.figure()
-plt.scatter(T,result,s=2);
-#plt.plot(T,result);
-plt.show();'''
 
 for i in range(len(b)):
 	ave=0
@@ -94,7 +74,10 @@ for i in range(len(b)):
 	m_ave.append(ave);
 	m_squ.append(squ);
 
-t=[]
+T=[]
+for i in range(200):
+	T.append(1.5+0.01*i)
+'''t=[]
 for i in range(len(b)):
 	corre=0
 	m_ave_before=0
@@ -129,7 +112,26 @@ plt.scatter(T,m_ave,s=20);
 plt.xlabel("$T/K$")
 plt.ylabel("$H$")
 plt.title("Condition $\mathcal{J}=-1,\mathcal{h}=0$")
+plt.show();'''
+m_ave=[m_ave[i]/3600 for i in range(len(m_ave))];
+def M_fit(Ts,Tcinf,beta,a):
+	return a*np.abs((-Ts+Tcinf)/Tcinf)**beta
+
+guess=[2.4,0.1,1];
+fita,fitb=curve_fit(M_fit,T[0:70],m_ave[0:70],guess);
+print(fita);
+plt.figure()
+plt.plot(T[0:70],[M_fit(T[i],fita[0],fita[1],fita[2]) for i in range(0,70)],color="r")
+plt.legend([r'$\alpha(\frac{T-T_c}{T_c})^\beta$'],
+           loc='upper right',
+           numpoints=1,
+           fancybox=False)
+plt.title("Transformation Point")
+plt.scatter(T,m_ave,s=10);
+plt.xlabel("$T/K$")
+plt.ylabel("$H$")
 plt.show();
+
 '''corre=0
 m_ave_before=0
 m_ave_after=0
@@ -146,3 +148,27 @@ print(m_ave_before*m_ave_after);
 print(m_squ[0]-pow(m_ave[0],2));
 print(10/(-np.log((corre-m_ave_before*m_ave_after)/(m_squ[0]-pow(m_ave[0],2)))));'''
 
+
+'''result=dict();
+r=b[0]
+for i in range(len(r)):
+	if r[i] in result:
+		result[r[i]]=result[r[i]]+1;
+	else:
+		result[r[i]]=1;
+
+x=[]
+y=[]
+num=len(b[0]);
+for x_1,y_1 in result.items():
+	x.append(x_1)
+	y.append(y_1/len(r));
+
+plt.figure()
+plt.ylim(0,max(y)*1.2)
+#plt.scatter(x,y,s=3)
+plt.bar([x[i]/3600 for i in range(len(x))],y,width=4/3600)
+plt.xlabel("Energy")
+plt.ylabel("Frequency")
+plt.title("The distribution of energy")
+plt.show()'''
